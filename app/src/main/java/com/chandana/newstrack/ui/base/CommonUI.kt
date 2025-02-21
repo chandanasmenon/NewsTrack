c0a6e33dc98aced2708192dfe0afd8c1a8bb77c7
chandanasmenon <chandanamenon2019@gmail.com>
2025-02-21 20:15:56 +0530

    Implement TopHeadlineSources Migrate to compose

--- app/src/main/java/com/chandana/newstrack/ui/base/CommonUI.kt
+++ app/src/main/java/com/chandana/newstrack/ui/base/CommonUI.kt
@@ -1,12 +1,29 @@
 package com.chandana.newstrack.ui.base
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.chandana.newstrack.R
+import com.chandana.newstrack.utils.extensions.capitalizeWords
+import com.chandana.newstrack.utils.extensions.getCountryName
+import com.chandana.newstrack.utils.extensions.getLanguageName
 
 @Composable
 fun ButtonText(text: String) {
@@ -18,3 +35,137 @@ fun ButtonText(text: String) {
         modifier = Modifier.padding(2.dp)
     )
 }
+
+@Composable
+fun ShowLoading() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        val contentDesc = stringResource(R.string.loading)
+        CircularProgressIndicator(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .semantics {
+                    contentDescription = contentDesc
+                }, color = MaterialTheme.colorScheme.primary
+        )
+    }
+}
+
+@Composable
+fun ShowError(text: String) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .background(MaterialTheme.colorScheme.errorContainer)
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.onErrorContainer,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(4.dp)
+        )
+    }
+}
+
+@Composable
+fun TitleText(title: String) {
+    if (title.isNotEmpty()) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.headlineLarge,
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier.padding(4.dp)
+        )
+    }
+}
+
+@Composable
+fun DescriptionText(description: String) {
+    if (description.isNotEmpty()) {
+        Text(
+            text = description,
+            style = MaterialTheme.typography.bodyMedium.copy(lineHeight = 20.sp),
+            color = MaterialTheme.colorScheme.onSurface,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 3,
+            modifier = Modifier.padding(4.dp)
+        )
+    }
+}
+
+@Composable
+fun CategoryDisplay(category: String) {
+    Row(
+        modifier = Modifier.padding(4.dp),
+        horizontalArrangement = Arrangement.spacedBy(5.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = stringResource(R.string.category),
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier.padding(2.dp)
+        )
+        Text(
+            text = category.capitalizeWords(),
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier.padding(2.dp)
+        )
+    }
+}
+
+@Composable
+fun LanguageDisplay(language: String) {
+    Row(
+        modifier = Modifier.padding(4.dp),
+        horizontalArrangement = Arrangement.spacedBy(5.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = stringResource(R.string.language),
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier.padding(2.dp)
+        )
+        Text(
+            text = language.getLanguageName(language),
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier.padding(2.dp)
+        )
+    }
+}
+
+@Composable
+fun CountryDisplay(country: String) {
+    Row(
+        modifier = Modifier.padding(4.dp),
+        horizontalArrangement = Arrangement.spacedBy(5.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = stringResource(R.string.country),
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier.padding(2.dp)
+        )
+        Text(
+            text = country.getCountryName(country),
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier.padding(2.dp)
+        )
+    }
+}
+
+
+
+
+
