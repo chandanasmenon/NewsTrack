f5fded7390195b80dd687a79bf955bcd040db4fc
chandanasmenon <chandanamenon2019@gmail.com>
2025-02-23 21:44:05 +0530

    Implement UI test for PagingTopHeadlineSources in Jetpack Compose

---
+++ app/src/androidTest/java/com/chandana/newstrack/pagingtopheadlinesources/PagingTopHeadlineSourcesTest.kt
@@ -0,0 +1,105 @@
+package com.chandana.newstrack.pagingtopheadlinesources
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertHasClickAction
+import androidx.compose.ui.test.hasScrollToNodeAction
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performScrollToNode
+import androidx.paging.PagingData
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.chandana.newstrack.data.model.ApiSource
+import com.chandana.newstrack.ui.pagingtopheadlinesources.PaginationTopHeadlineScreen
+import kotlinx.coroutines.flow.flowOf
+import org.junit.Rule
+import org.junit.Test
+
+class PagingTopHeadlineSourcesTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun getPagingData_whenUiStateIsSuccess_isShown() {
+        val pagingData = PagingData.from(testSources)
+        val flow = flowOf(pagingData)
+
+        composeTestRule.setContent {
+            PaginationTopHeadlineScreen(
+                sources = flow.collectAsLazyPagingItems(),
+                onNewsClick = {}
+            )
+        }
+
+        composeTestRule
+            .onNodeWithText(testSources[0].name, substring = true)
+            .assertExists()
+            .assertHasClickAction()
+
+        composeTestRule.onNode(hasScrollToNodeAction())
+            .performScrollToNode(hasText(testSources[5].name, substring = true))
+
+        composeTestRule
+            .onNodeWithText(testSources[5].name, substring = true)
+            .assertExists()
+            .assertHasClickAction()
+    }
+}
+
+private val testSources = listOf(
+    ApiSource(
+        category = "Category1",
+        country = "Country1",
+        language = "Language1",
+        name = "Name1",
+        description = "Description1",
+        url = "Url1",
+        id = "Id1"
+    ),
+    ApiSource(
+        category = "Category2",
+        country = "Country2",
+        language = "Language2",
+        name = "Name2",
+        description = "Description2",
+        url = "Url2",
+        id = "Id2"
+    ),
+    ApiSource(
+        category = "Category3",
+        country = "Country3",
+        language = "Language3",
+        name = "Name3",
+        description = "Description3",
+        url = "Url3",
+        id = "Id3"
+    ),
+    ApiSource(
+        category = "Category4",
+        country = "Country4",
+        language = "Language4",
+        name = "Name4",
+        description = "Description4",
+        url = "Url4",
+        id = "Id4"
+    ),
+    ApiSource(
+        category = "Category5",
+        country = "Country5",
+        language = "Language5",
+        name = "Name5",
+        description = "Description5",
+        url = "Url5",
+        id = "Id5"
+    ),
+    ApiSource(
+        category = "Category6",
+        country = "Country6",
+        language = "Language6",
+        name = "Name6",
+        description = "Description6",
+        url = "Url6",
+        id = "Id6"
+    )
+)
